dec8a1f7380232d52400a41d9e6f4cc0f142aec7
bnyf <liyufeng614@126.com>
2021-01-16 23:14:55 +0800

    add object

---
+++ src/object/obj_thread.cc
@@ -0,0 +1,5 @@
+//
+// Created by 李煜峰 on 2021/1/16.
+//
+
+#include "obj_thread.h"
---
+++ src/object/obj_thread.h
@@ -0,0 +1,14 @@
+//
+// Created by 李煜峰 on 2021/1/16.
+//
+
+#ifndef BERRYSCRIPT_OBJ_THREAD_H
+#define BERRYSCRIPT_OBJ_THREAD_H
+
+
+class obj_thread {
+
+};
+
+
+#endif //BERRYSCRIPT_OBJ_THREAD_H
